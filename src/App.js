519929c3290eeadea40d5c778f904a783be4b949
jshepdevelopment <jshepherd@outlook.com>
2020-09-11 23:49:11 -0600

    Update reactjsapp demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import FunctionalComp from "./Components/FunctionalComp" 
+import {ClassComp, ClassComp1} from "./Components/ClassComp" 
+import Click from "./Components/Click"
+import Counter from "./Components/Counter"
+import ParentComp from "./Components/ParentComp"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello and welcome to ReactJS Components Demonstration</h1>
+      <FunctionalComp />
+      <ClassComp />
+      <ClassComp1 />
+      <Click />
+      <Counter />
+      <ParentComp />
     </div>
   );
 }
